741aaf8acd0993e67b90aae9f790065f8e055858
Jeremy <markjeremyj@gmail.com>
2025-07-23 21:04:44 +0300

    adds importing to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 
 import React from 'react';
 import LoginPage from './Components/LoginPage/LoginPage';
+import ContactUs from './Components/ContactUsPage';
 
 
 
 function App() {
 
-  function onSubmit(event) {
-    event.preventDefault();
-    // Handle form submission logic here
-    console.log('Form submitted');
-  }
+
 
 
   return (
     <div>
       {/* Add your content here */}
       <h1>Federal Reserve App</h1>
-      <LoginPage onSubmit={onSubmit} />
+      <LoginPage  />
+      <ContactUs />
     </div>
   )
 }
