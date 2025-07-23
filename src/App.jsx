7c71154e9a456d39e0cda3d2c8a130b6235bf5b1
Jeremy <markjeremyj@gmail.com>
2025-07-23 19:00:48 +0300

    adds some styling features

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ import LoginPage from './Components/LoginPage/LoginPage';
 
 function App() {
 
+  function onSubmit(event) {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log('Form submitted');
+  }
+
 
   return (
     <div>
       {/* Add your content here */}
       <h1>Federal Reserve App</h1>
-      <LoginPage />
+      <LoginPage onSubmit={onSubmit} />
     </div>
   )
 }
