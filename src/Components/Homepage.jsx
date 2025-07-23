9b85ee586a05f792a0db1432aa0010540e2df579
nicholas.kiama <nicholas.kiama@student.moringaschool.com>
2025-07-23 17:38:13 +0300

    first commit

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -12,25 +12,7 @@ function Homepage() {
         </ul>
       </nav>
 
-      <div className="content">
-        <h1>Welcome to the Federal Reserve Watchlist</h1>
-        <p>This site provides information on individuals monitored by the FBI and the PRIZE you can recieve for any useful info given about their whereabouts. !AMA NAMNA GANI MY FRIEND!</p>
-
-        <div className="fbi-images">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu6MZ5e8xfTEGmUdEia7F_5c2-gkJdA5JtxQ&s"
-            alt="FBI Seal"
-          />
-          <img
-            src="https://www.shutterstock.com/image-vector/yellow-black-color-line-striped-260nw-2446550797.jpg"
-            alt="FBI Wanted"
-          />
-        </div>
-      </div>
-
-      <footer className="footer">
-        <p>© 2025 Federal Reserve Project</p>
-      </footer>
+     
     </div>
   );
 }
