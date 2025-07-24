a44d3fcbc179b61c3628d52e45ed3afe76e30e9a
Jeremy <markjeremyj@gmail.com>
2025-07-24 12:57:48 +0300

    solves conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
 import LoginPage from './Components/LoginPage/LoginPage';
 import ContactUs from './Components/ContactUsPage';
-<<<<<<< HEAD
-import Homepage from './Components/Homepage';
-import Aboutus from './Components/AboutUs';
 
-=======
 import Home from './Components/Home.jsx';
 import { Routes, Route } from 'react-router-dom';
 import Details from './pages/Details.jsx';
 import CriminalsDisplay from './pages/CriminalsDisplay.jsx';
 import AboutUs from './Components/AboutUs.jsx'
->>>>>>> origin/main
+
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
 
-      <LoginPage  />
-
-      <ContactUs />
-
-      <Homepage />
-      <Aboutus />
-
-
-=======
       {/* <LoginPage  /> */}
     <Routes>
       <Route >
@@ -37,7 +23,6 @@ function App() {
         <Route path="contactus" element={<ContactUs />} />
       </Route>
     </Routes>
->>>>>>> origin/main
     </div>
   )
 }
